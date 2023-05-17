ff3647a6198001a4047bc49d5a846b47ffb1756c
CatMan <garfield_lv@163.com>
2023-05-17 09:34:04 +0800

    修改buffline 配置

--- lua/catman/plugins/bufferline.lua
+++ lua/catman/plugins/bufferline.lua
@@ -7,6 +7,11 @@ end
 bufferline.setup({
 
 	options = {
+		buffer_close_icon = "󰅖",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
 		-- 使用 nvim 内置lsp
 		diagnostics = "nvim_lsp",
 		-- 左侧让出 nvim-tree 的位置
