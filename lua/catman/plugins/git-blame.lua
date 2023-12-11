67a4f19c5b3be5d699a4400fc57f05a3ff76348d
CatMan <garfield_lv@163.com>
2023-12-11 17:27:54 +0800

    add { "vim-visual-multi" },

--- lua/catman/plugins/git-blame.lua
+++ lua/catman/plugins/git-blame.lua
@@ -1,5 +1,3 @@
-vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
-local git_blame = require("gitblame")
 return {
 	"f-person/git-blame.nvim",
 	event = { "BufReadPre", "BufNewFile" },
@@ -7,12 +5,4 @@ return {
 	opts = function(_, opts)
 		vim.g.gitblame_date_format = "%Y-%m-%d %H:%M:%S"
 	end,
-
-	require("lualine").setup({
-		sections = {
-			lualine_c = {
-				{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
-			},
-		},
-	}),
 }
